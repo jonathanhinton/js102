faea9cc9913b10c5f7c509df91d792182f656be7
Jonathan Hinton <jonathanhinton@Jonathans-MacBook-Pro.local>
2015-08-17 17:19:50 -0500

    remove instructions, add update

--- main.js
+++ main.js
@@ -1,23 +1,7 @@
 //CURRENT RESULTS AND COMMENTS:
 // I was able to get all html functionality working as well as get the button to activate the function.  I could not get the updated array to live outside the scope of the function. I will continue to work on this over the weekend.
-/*
-  4. Write a JavaScript function that accepts an array as an argument, and returns a new array that is sorted.
-  5. Write a JavaScript function that accepts an array, and returns a new array that contains elements in the array you passed in that are larger than 25.
 
-
-  8. When the button is clicked retrieve the value of the input box. You should enter in a comma-delimited list of integers (e.g. 10,3,5,67,22,etc..).
-  9. Convert that string of comma-delimited numbers into an array.
-  10. Pass that array into the function that finds numbers higher than 25.
-  11. The array that is the result of that, pass it into the function that sorts the array.
-  12. Loop over the sorted array and create a DOM string that wraps a <div> element around each number (e.g. output += something).
-
-  13. Give each <div> a class of "number".
-  14. Write a CSS class that changes the font color of the even elements to red.
-  15. Write a CSS class that changes the background color of the odd elements to azure.
-
-  16. Push all of your code to Github.
-  17. Send the link to your Github project to your assigned TA.
-*/
+// UPDATE 8/17 GOT IT WORKING!
 
 
 var button = document.getElementById("submit")
